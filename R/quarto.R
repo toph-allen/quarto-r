3f21346ab2c4a6c9148934f666d138a9ddbbc2ba
Toph Allen <toph.allen@gmail.com>
2022-04-05 16:26:45 -0400

    Expand quarto_path function to use fallbacks

--- R/quarto.R
+++ R/quarto.R
@@ -8,12 +8,30 @@
 #' @export
 quarto_path <- function() {
   path_env <- Sys.getenv("QUARTO_PATH", unset = NA)
-  if (is.na(path_env)) {
-    path <- unname(Sys.which("quarto"))
-    if (nzchar(path)) path else NULL
-  } else {
-    path_env
+  if (!is.na(path_env)) {
+    return(path_env)
   }
+  # Fallback: check well-known locations if no QUARTO_PATH set.
+  locations <- c(
+    # Location on PATH
+    "quarto",
+
+    # A location used by some installers
+    "/usr/local/bin/quarto",
+
+    # A location used by some installers
+    "/opt/quarto/bin/quarto",
+
+    # The IDE's embedded Quarto on macOS
+    "/Applications/RStudio.app/Contents/MacOS/quarto/bin/quarto"
+  )
+  for (location in locations) {
+    path <- unname(Sys.which(location))
+    if (nzchar(path)) {
+      return(path)
+    }
+  }
+  return(NULL)
 }
 
 find_quarto <- function() {
